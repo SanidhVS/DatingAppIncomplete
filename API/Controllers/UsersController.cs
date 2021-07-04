4526aa58be4e511fec2d41178a2641db60358991
sanidh.vs@experionglobal.com <sanidh.vs@experionglobal.com>
2021-07-04 22:22:57 +0530

    Initial commit dating app

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()   //Asyncing help from a scalabilty point of view, for asyncing the API should be a task
+        {
+            var users = _context.Users.ToListAsync();
+            return await users;  //Await for every async
+        }
+
+        //api/users/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+ 
+        }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext  //To use Entity framework
+    {
+        public DataContext(DbContextOptions options) : base(options) //constructor-when the class is callled this is initialized 
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; } //To create a class for which we need database for (Users is the table)
+    }
+}
